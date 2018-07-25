dd37d54b69c1b2927bca0d856834bad8fd2f6702
Yang Dai <yangdai713@aim.com>
2018-07-25 14:55:34 -0400

    styled html, and ready to deploy

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     # My apps
     'learning_logs',
     'users',
+
+    # Thrid party apps
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,8 @@ STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
